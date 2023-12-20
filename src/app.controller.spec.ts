91e565efbd0d0f8b7984dcb054a7bfe9beabb745
Fermin Blanco <luillyfe89@gmail.com>
2023-12-20 18:54:07 -0500

    refactor: remove prediction service from root module

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,30 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { BadRequestException } from '@nestjs/common';
 
 import { AppService } from './app.service';
-import { ConcurrentService } from './prediction/concurrent/concurrent.service';
 
 import { AppController } from './app.controller';
 
 describe('AppController', () => {
   let appController: AppController;
-  let concurrentService: ConcurrentService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [
-        AppService,
-        {
-          provide: ConcurrentService,
-          useValue: {
-            predict: jest.fn(),
-          },
-        },
-      ],
+      providers: [AppService],
     }).compile();
 
-    concurrentService = app.get<ConcurrentService>(ConcurrentService);
     appController = app.get<AppController>(AppController);
   });
 
@@ -32,28 +20,6 @@ describe('AppController', () => {
     it('should return a message for the  endpoint', () => {
       const response = appController.getHello();
       expect(response.message).toEqual('root endpoint for the prediction API');
-    });
-  });
-
-  describe('predict', () => {
-    it('should return an error if the text input is empty', async () => {
-      const text = '';
-      try {
-        await appController.predict(text);
-      } catch (error) {
-        expect(error).toBeInstanceOf(BadRequestException);
-        expect(error.message).toEqual('Please enter a valid text input');
-      }
-    });
-
-    it('should return the output of the predict method from the concurrentService', async () => {
-      const text = 'test';
-      const expectedOutput = 'test output';
-      jest.spyOn(concurrentService, 'predict').mockImplementation(async () => {
-        return expectedOutput;
-      });
-      const response = await appController.predict(text);
-      expect(response).toEqual(expectedOutput);
     });
   });
 });
