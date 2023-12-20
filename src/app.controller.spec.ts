f1d46bb7c272fb9cb4eb3c7998a571b11b4cc984
Fermin Blanco <luillyfe89@gmail.com>
2023-12-20 01:44:24 -0500

    chore: adding test cases for the predict API on app controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,10 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
+import { BadRequestException } from '@nestjs/common';
+
 import { AppService } from './app.service';
 import { ConcurrentService } from './prediction/concurrent/concurrent.service';
 
+import { AppController } from './app.controller';
+
 describe('AppController', () => {
   let appController: AppController;
+  let concurrentService: ConcurrentService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -20,15 +24,36 @@ describe('AppController', () => {
       ],
     }).compile();
 
+    concurrentService = app.get<ConcurrentService>(ConcurrentService);
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      const response = {
-        message: 'root endpoint for the prediction API',
-      };
-      expect(appController.getHello()).toStrictEqual(response);
+    it('should return a message for the  endpoint', () => {
+      const response = appController.getHello();
+      expect(response.message).toEqual('root endpoint for the prediction API');
+    });
+  });
+
+  describe('predict', () => {
+    it('should return an error if the text input is empty', async () => {
+      const text = '';
+      try {
+        await appController.predict(text);
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(error.message).toEqual('Please enter a valid text input');
+      }
+    });
+
+    it('should return the output of the predict method from the concurrentService', async () => {
+      const text = 'test';
+      const expectedOutput = 'test output';
+      jest.spyOn(concurrentService, 'predict').mockImplementation(async () => {
+        return expectedOutput;
+      });
+      const response = await appController.predict(text);
+      expect(response).toEqual(expectedOutput);
     });
   });
 });
