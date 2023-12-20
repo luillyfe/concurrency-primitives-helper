db77f50c6475fbf4dfd0a065f743c5318fd96120
Fermin Blanco <luillyfe89@gmail.com>
2023-12-20 00:14:38 -0500

    chore: adding prediction module to the main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
+import { PredictionModule } from './prediction/prediction.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.development.env' })],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.development.env' }),
+    PredictionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
