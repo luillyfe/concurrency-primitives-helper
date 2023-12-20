1bbaf36bdab4735e49abb2adf3b7ca25d24164aa
Fermin Blanco <luillyfe89@gmail.com>
2023-12-20 18:55:24 -0500

    feature: formatting prediction response to json.

--- src/prediction/prediction.module.ts
+++ src/prediction/prediction.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { ConcurrentService } from './concurrent/concurrent.service';
+import { PredictionController } from './prediction/prediction.controller';
 
 @Module({
   imports: [ConfigModule],
   providers: [ConcurrentService],
-  exports: [ConcurrentService],
+  controllers: [PredictionController],
 })
 export class PredictionModule {}
